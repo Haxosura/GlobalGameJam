1bd214835a7be1e4b4e0a3b376dbd355a13bee3e
st0reytime <ethanjstorey@icloud.com>
2024-01-22 16:05:35 +0000

    Basic Enemy Patrol
    NPC now patrol a set individual path. Will do random patrol next.

---
+++ Source/GlobalGameJam/MyAIController.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+#include "MyNPC.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Hearing.h"
+#include "GlobalGameJamCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AMyAIController::AMyAIController(FObjectInitializer const& ObjectInitializer)
+{
+	SetupPerceptionSystem();
+}
+
+void AMyAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	if (AMyNPC* const npc = Cast<AMyNPC>(InPawn))//Check we are possessing enemy
+	{
+		if (UBehaviorTree* const tree = npc->GetBehaviorTree())//Check if behavior tree is there
+		{
+			UBlackboardComponent* b;
+			UseBlackboard(tree->BlackboardAsset, b);
+			Blackboard = b;
+			RunBehaviorTree(tree);
+		}
+	}
+}
+
+void AMyAIController::SetupPerceptionSystem()
+{
+	//Figure out what to detect later
+}
+
+void AMyAIController::OnTargetDetected(AActor* Actor, FAIStimulus const Stimulus)
+{
+	//Figure out what to detect later
+}
