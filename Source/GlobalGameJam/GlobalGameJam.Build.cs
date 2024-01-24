77ec94f8f4f6e3ebe0a3d34485d4ba531469c00f
Haxosura <123653087+Haxosura@users.noreply.github.com>
2024-01-24 13:02:32 +0000

    Updated Build File

--- Source/GlobalGameJam/GlobalGameJam.Build.cs
+++ Source/GlobalGameJam/GlobalGameJam.Build.cs
@@ -8,6 +8,6 @@ public class GlobalGameJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "EnhancedInput", "GameplayTasks" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "EnhancedInput", "GameplayTasks" , "UMG" });
     }
 }
